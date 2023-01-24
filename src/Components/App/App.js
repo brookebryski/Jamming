7008105a367655ac8462c9c57f59ba071e814712
BrookeBryski <brookeabryski@gmail.com>
2023-01-24 14:33:18 -0800

    Track components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
+
 class App extends React.Component {
   render() {
     return (
@@ -9,10 +13,10 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          {/* <SearchBar /> */}
+          <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults /> */}
-            {/* <Playlist /> */}
+            <SearchResults /> 
+            <Playlist />
           </div>
         </div>
       </div>
