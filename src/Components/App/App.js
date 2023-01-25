722f422d8ac6e134fea8b656f233b4c37930043e
BrookeBryski <brookeabryski@gmail.com>
2023-01-25 12:42:27 -0800

    added search results state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,15 @@ import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      searchResults: [{ name: 'Dissolving', artist: 'Hannah Cohen', album: 'Welcome Home', id: 1},
+      { name: 'Paprika', artist: 'Japenese Breakfast', album: 'Jubilee', id: 2}, { name: 'Drunk in LA', artist: 'Beach House', album: 'Seven', id: 3}
+    ]
+    }
+  }
   render() {
     return (
       <div>
@@ -15,7 +24,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults /> 
+            <SearchResults searchResults={this.state.searchResults}/> 
             <Playlist />
           </div>
         </div>
